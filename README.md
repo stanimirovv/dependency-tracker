aef8a9c33d64c2b71cf85cd20b986f02f1e3ca67
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2023-01-29 20:29:29 +0200

    fix: decay score calculation

--- README.md
+++ README.md
@@ -1,36 +1,35 @@
 # Dependency tracker
 
-A light weight, configurable script that counts how many versions behind the project is from its dependencies.
+A light weight, configurable script that generates a score depending on how many versions behind the project is from its dependencies.
 
 This is not a replacement for `npm outdated` 
-It offers similar functionality from a different angle. The goal is to be able to use this script as a validator for CI/CD pipelines.
-Main difference is that this script will not print the latest version of each dependency, but rather how many versions behind the project is for each dependency.
-Also unlike npm outdated this script is configurable to have a tolerance for how many versions behind the project is allowed to be and can ignore major/minor/patch versions when checking for versions behind.
-You can even skip specific packages, see the USAGE section.
 
-The script will check the following:
-- How many versions behind the project is for each dependency
-- How many major, minor and patch versions it is behind for each dependency
-- Which is the latest and greatest version of each dependency
-
-The script will print a report on STDOUT in json format containing the information above.
+The script has 2 primary use cases:
+- Exploration purposes to understand how many versions behind the project is from its dependencies.
+- Use it as a guard in CI/CD pipelines to fail if the project is not up to date with its dependencies.
 
 ## Features
 
-The script can be used as a validator for CI/CD pipelines by checking the exit code of the script.
+Print a JSON report with a dacay score and the packages and their versions behind.
+
+Use standard bash exit code:
 If the exit code is 0, the project is up to date with its dependencies.
 If the exit code is 1, the project is not up to date with its dependencies.
+
+### Configuration
 Use the envvar `VERSIONS_BEHIND_THRESHOLD=<max versions behind int>` to enable the non 0 exit statuses.
 Set `VERSIONS_BEHIND_THRESHOLD=0` to fail on any version behind.
 You can also skip major/minor/patch versions by setting the envvars `SKIP_MAJOR_VERSIONS=1` and `SKIP_MINOR_VERSIONS=1` and `SKIP_PATCH_VERSIONS=1`. 
 Skips dev/alpha/beta/rc versions.
 
+### Decay score
 The script generates an overall `decay score` for the project.
 The dependency score is the sum of all dependency versions behind.
 1000 for major versions behind, 100 for minor and 1 for patch.
 Patch is considered less important as they are released significantly more often than major/minor versions.
 
 If for readability or scripting purposes you want the script to return only the decay score, you can use the envvar `ONLY_DECAY=1`.
+
 You can also use `DECAY_THRESHOLD=<max decay score int>` to set a threshold for the decay score and have the script return a non 0 exit status if the decay score is above the threshold.
 
 By default only the prd dependencies are checked.
@@ -70,7 +69,12 @@ PACKAGES_TO_SKIP='["typescript"]' npx @stanimirovv/dependency-tracker /path/to/p
 Have a max decay score of 10000 and skip minor versions:
 ```bash
 DECAY_THRESHOLD=10000 SKIP_MINOR_VERSIONS=1 npx @stanimirovv/dependency-tracker /path/to/package-lock.json
+```
 
-## Work log
-29/01/2022 Some packages like TypeScript spam major versions. Should the script detect if the version is the same as the previous one and skip it?
-it can be found in dist-tags.latest from npm view
+## Example report
+```json
+{"decayScore":1,"packageReports":[{"packageName":"reflect-metadata","versionsBehind":1,"minorVersionsBehind":0,"patchVersionsBehind":1,"majorVersionsBehind":0,"currentVersion":"0.1.12","latestVersion":"0.1.13"},{"packageName":"@types/bcrypt","versionsBehind":0,"minorVersionsBehind":0,"patchVersionsBehind":0,"majorVersionsBehind":0,"currentVersion":"^5.0.0","latestVersion":"5.0.0"}]}
+```
+
+ ## Work Log
+ 2022/01/29 - generatePackageReport must be refactored and simplified. We need more tests for it.
--- src/generatePackageReport.ts
+++ src/generatePackageReport.ts
@@ -7,60 +7,63 @@ export default function generatePackageReport(
   { versions, currentVersion, packageName, latestVersion }: PackageWithVersions,
   reportConfig: ReportConfig
 ): PackageVersionReport {
+  const originalCurrentVersion = currentVersion;
   let minorVersionsBehind = 0;
   let patchVersionsBehind = 0;
   let majorVersionsBehind = 0;
 
-  const versionsBehindArray = versions.filter((version: string) => {
+  versions.forEach((version: string) => {
+    if (isUnstableVersion(version)) {
+      return;
+    }
+
     // extract versions
     const majorVersion = version.split(".")[0];
-    const minorVersion = `${version.split(".")[0]}${version.split(".")[1]}`;
+    const minorVersion = `${version.split(".")[0]}.${version.split(".")[1]}`;
     const patchVersion = version;
 
     // extract current versions
     const currentMajorVersion = currentVersion.split(".")[0];
-    const currentMinorVersion = `${currentVersion.split(".")[0]}${
+    const currentMinorVersion = `${currentVersion.split(".")[0]}.${
       currentVersion.split(".")[1]
     }`;
     const currentPatchVersion = currentVersion;
 
     // Calculate which versions are behind
-    const hasBiggerMajorVersion =
-      compareVersions(majorVersion, currentMajorVersion) === 1;
-    const hasBiggerMinorVersion =
-      compareVersions(minorVersion, currentMinorVersion) === 1;
-    const hasBiggerPatchVersion =
-      compareVersions(patchVersion, currentPatchVersion) === 1;
 
     const isVersionAheadOfLatest =
-      compareVersions(currentMajorVersion, latestVersion) != -1;
+      compareVersions(version, latestVersion) === 1;
 
-    // Increment the versions behind
-    const shouldIncrementMajor =
-      hasBiggerMajorVersion &&
-      !reportConfig.skipMajorVersions &&
-      !isVersionAheadOfLatest;
-    if (shouldIncrementMajor) {
+    const incrementPatch = shouldIncrementPatch(
+      patchVersion,
+      currentPatchVersion,
+      reportConfig.skipPatchVersions,
+      isVersionAheadOfLatest
+    );
+    const incrementMinor = shouldIncrementMinor(
+      minorVersion,
+      currentMinorVersion,
+      reportConfig.skipMinorVersions,
+      isVersionAheadOfLatest
+    );
+
+    const incrementMajor = shouldIncrementMajor(
+      majorVersion,
+      currentMajorVersion,
+      reportConfig.skipMajorVersions,
+      isVersionAheadOfLatest
+    );
+
+    if (incrementMajor) {
       majorVersionsBehind++;
-    }
-
-    const shouldIncrementMinor =
-      hasBiggerMinorVersion &&
-      !reportConfig.skipMinorVersions &&
-      !isVersionAheadOfLatest;
-    if (shouldIncrementMinor) {
+    } else if (incrementMinor) {
       minorVersionsBehind++;
-    }
-
-    const shouldIncrementPatch =
-      hasBiggerPatchVersion &&
-      !reportConfig.skipPatchVersions &&
-      !isVersionAheadOfLatest;
-    if (shouldIncrementPatch) {
+    } else if (incrementPatch) {
       patchVersionsBehind++;
     }
-
-    return version > currentVersion;
+    if (incrementMajor || incrementMinor || incrementPatch) {
+      currentVersion = version;
+    }
   });
 
   const versionsBehind =
@@ -72,7 +75,57 @@ export default function generatePackageReport(
     minorVersionsBehind,
     patchVersionsBehind,
     majorVersionsBehind,
-    currentVersion,
+    currentVersion: originalCurrentVersion,
     latestVersion,
   };
+}
+
+function shouldIncrementMajor(
+  majorVersion: string,
+  currentMajorVersion: string,
+  skipMajorVersions: boolean,
+  isVersionAheadOfLatest: boolean
+) {
+  const hasBiggerMajorVersion =
+    compareVersions(majorVersion, currentMajorVersion) === 1;
+  return hasBiggerMajorVersion && !skipMajorVersions && !isVersionAheadOfLatest;
+}
+
+function shouldIncrementMinor(
+  minorVersion: string,
+  currentMinorVersion: string,
+  skipMinorVersions: boolean,
+  isVersionAheadOfLatest: boolean
+) {
+  const hasBiggerMinorVersion =
+    compareVersions(minorVersion, currentMinorVersion) === 1;
+  return hasBiggerMinorVersion && !skipMinorVersions && !isVersionAheadOfLatest;
+}
+
+function shouldIncrementPatch(
+  patchVersion: string,
+  currentPatchVersion: string,
+  skipPatchVersions: boolean,
+  isVersionAheadOfLatest: boolean
+) {
+  const hasBiggerPatchVersion =
+    compareVersions(patchVersion, currentPatchVersion) === 1;
+  return hasBiggerPatchVersion && !skipPatchVersions && !isVersionAheadOfLatest;
+}
+
+function isUnstableVersion(version: string): boolean {
+  const unstableKeywords = [
+    "alpha",
+    "beta",
+    "rc",
+    "next",
+    "dev",
+    "canary",
+    "insiders",
+    "preview",
+    "exp",
+  ];
+  return (
+    unstableKeywords.filter((keyword) => version.includes(keyword)).length > 0
+  );
 }
